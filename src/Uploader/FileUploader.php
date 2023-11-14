3dd4909c6d08e530c56d0f38d0c687aa4f9c9fcd
Nur Hidayatullah <>
2023-11-14 14:55:33 +0700

    compress

--- src/Uploader/FileUploader.php
+++ src/Uploader/FileUploader.php
@@ -13,32 +13,40 @@ use Symfony\Component\String\Slugger\SluggerInterface;
  */
 class FileUploader implements UploaderInterface
 {
+
+    /**
+     *
+     * @var string
+     */
     private $targetDirectory;
+
+    /**
+     *
+     * @var SluggerInterface
+     */
     private $slugger;
 
-    public function __construct($targetDirectory, SluggerInterface $slugger)
+    public function __construct(string $targetDirectory, SluggerInterface $slugger)
     {
         $this->targetDirectory = $targetDirectory;
         $this->slugger = $slugger;
     }
 
-    public function upload(UploadedFile $file, string $directory = null):?File
+    public function upload(UploadedFile $file, string $directory = null, bool $compress = true):?File
     {
         $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = strtolower($this->slugger->slug($originalFilename));
-        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        $fileName = sprintf("%s-%s.%s", $safeFilename, uniqid(), $file->guessExtension());
 
         $targetDir = $directory ? $this->targetDirectory . DIRECTORY_SEPARATOR . $directory : $this->targetDirectory;
         $fileSystem = new Filesystem();
-        
         try {
-            if(!$fileSystem->exists($targetDir))
-            {
+            if (!$fileSystem->exists($targetDir)) {
                 $fileSystem->mkdir($targetDir, 0777);
             }
-            
+
             return $file->move($targetDir, $fileName);
-            
+
         } catch (FileException $e) {
             throw $e;
         }
--- src/Uploader/UploaderInterface.php
+++ src/Uploader/UploaderInterface.php
@@ -9,8 +9,8 @@ use Symfony\Component\HttpFoundation\File\File;
  * @author Nur Hidayatullah <kematjaya0@gmail.com>
  */
 interface UploaderInterface {
-    
-    public function upload(UploadedFile $file, string $directory = null):?File;
-    
+
+    public function upload(UploadedFile $file, string $directory = null, bool $compress = true):?File;
+
     public function getTargetDirectory():?string;
 }
